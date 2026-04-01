bba0f865423dc5e870fb11f4b842276a3e544b90
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2026-04-02 01:40:36 +0800

    remove hard-coded port

--- app/src/main/kotlin/com.example/Database.kt
+++ app/src/main/kotlin/com.example/Database.kt
@@ -8,7 +8,7 @@ object Database {
     private val dbUser = System.getenv("DB_USER") ?: error("DB_USER is not set")
     private val dbPassword = System.getenv("DB_PASSWORD") ?: ""
     private val dbHost = System.getenv("DB_HOST") ?: error("DB_HOST is not set")
-    private val dbPort = System.getenv("DB_PORT") ?: "3306"
+    private val dbPort = System.getenv("DB_PORT") ?: ""
     private val dbName = System.getenv("DB_NAME") ?: error("DB_NAME is not set")
 
     private val URL = "jdbc:mysql://$dbHost:$dbPort/$dbName?sslMode=REQUIRED"
